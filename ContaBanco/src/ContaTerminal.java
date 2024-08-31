a46d271d37ab29fb0c9c2fb26f3ee994505a7396
TacianoAssis <tacianocanassa@gmail.com>
2024-08-30 23:36:27 -0300

    Update ContaTerminal.java

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -3,23 +3,69 @@ import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) {
-        int numero;
-        double saldo;
-        String agencia, nomeCliente;
         Scanner scan = new Scanner(System.in).useLocale(Locale.US);
+        String nomeCliente = "";
+        do{
+            System.out.print("Por favor digite o seu nome: ");
+            try {
+                nomeCliente = scan.nextLine().trim();
+                if(nomeCliente.isEmpty()) {
+                    throw new IllegalArgumentException("O nome não pode ser vazio!");
+                }
+            }catch (IllegalArgumentException e){
+                System.out.println("Entrada inválida! Erro: " + e.getMessage());
+            }
+        }while(nomeCliente.isEmpty());
 
-        System.out.println("Por favor digite o seu nome:");
-        nomeCliente = scan.next();
-        System.out.println("Por favor digite o seu sobrenome:");
-        nomeCliente = nomeCliente.concat(" " + scan.next())   ;
-        System.out.println("Por favor digite o código de sua agência:");
-        agencia = scan.next();
-        System.out.println("Por favor digite o número da sua conta:");
-        numero = scan.nextInt();
-        System.out.println("Por favor digite o valor de depósito:");
-        saldo = scan.nextDouble();
+        String agencia = "";
+        do{
+            System.out.print("Por favor digite o número da sua agência: ");
+            try {
+                agencia = scan.nextLine().trim();
+                if(agencia.isEmpty()) {
+                    throw new IllegalArgumentException("O número da agência não pode ser vazio!");
+                }
+            }catch (IllegalArgumentException e){
+                System.out.println("Entrada inválida! Erro: " + e.getMessage());
+            }
+        }while(agencia.isEmpty());
 
-        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero +
-                " e seu saldo R$ " + saldo + ", ja está disponível para saque");
+        int numeroConta = 0;
+        do{
+            System.out.print("Por favor digite o número da sua conta: ");
+            try {
+                numeroConta =  Integer.parseInt(scan.nextLine().trim());
+
+                if(numeroConta <= 0) {
+                    throw new IllegalArgumentException("O número da conta não pode ser 0 ou um número negativo!");
+                }
+            } catch (NumberFormatException e){
+                System.out.println("Entrada inválida! Erro: informe um número de conta válido! Exemplo: '1234' ");
+
+            } catch (IllegalArgumentException e){
+                System.out.println("Entrada inválida! Erro: " + e.getMessage());
+            }
+        }while(numeroConta <= 0);
+
+        double saldoConta = 0.0;
+        do{
+            System.out.print("Por favor digite o valor de depósito no formato xx.xx: ");
+            try{
+                saldoConta = Double.parseDouble(scan.nextLine());
+                if(saldoConta <= 0){
+                    throw new IllegalArgumentException("O saldo da conta deve ser um valor positivo e maior que zero!");
+                }
+            }catch (NumberFormatException e){
+                System.out.println("Entrada inválida! Erro: Informe um saldo no formato XX.XX");
+            }catch (IllegalArgumentException e){
+                System.out.println("Entrada inválida! Erro: " + e.getMessage());
+            }
+        }while (saldoConta <= 0);
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco!\nSua agência é " + agencia + ", conta " + numeroConta +
+                " e seu saldo R$ " + saldoConta + " ja está disponível para saque!");
+        scan.close();
+        System.out.println("Fim!");
     }
 }
+
